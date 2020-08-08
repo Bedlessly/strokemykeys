a217ca2e2846a554715483caa88e9a4dca064e87
Bedlessly <jejojora2@gmail.com>
2020-08-08 18:38:20 -0400

    Update Reference.java

--- src/main/java/io/github/zenyiar/strokemykeys/Reference.java
+++ src/main/java/io/github/zenyiar/strokemykeys/Reference.java
@@ -1,11 +1,11 @@
 package io.github.zenyiar.strokemykeys;
 
 /**
- * Created by Zenyiar
+ * Created by Bedlessly
  */
 public class Reference 
 {
-    public static final String MODID = "zenyiars_strokemykeys";
-    public static final String VERSION = "1.0";
-    public static final String VERSION_RELEASE_DATE = "May 1st, 2020";
+    public static final String MODID = "strokemykeys";
+    public static final String VERSION = "1.`";
+    public static final String VERSION_RELEASE_DATE = "August 8th, 2020";
 }
