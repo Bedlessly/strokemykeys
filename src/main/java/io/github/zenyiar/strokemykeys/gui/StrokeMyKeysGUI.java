bbb1efc250d2c660ae716c1f78084c4a4e4c6f12
Bedlessly <jejojora2@gmail.com>
2020-08-08 18:37:19 -0400

    Update and rename StrokeMyKeysGui.java to StrokeMyKeysGUI.java

---
+++ src/main/java/io/github/zenyiar/strokemykeys/gui/StrokeMyKeysGUI.java
@@ -0,0 +1,68 @@
+package io.github.zenyiar.strokemykeys.gui;
+
+import club.sk1er.elementa.components.UIText;
+import club.sk1er.elementa.components.Window;
+import club.sk1er.elementa.constraints.ConstantColorConstraint;
+import club.sk1er.elementa.constraints.PixelConstraint;
+import io.github.zenyiar.strokemykeys.sStrokeMyKeysConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import org.lwjgl.input.Keyboard;
+
+import java.awt.*;
+import java.util.Properties;
+
+public class StrokeMyKeysGUI extends GuiScreen
+{
+    public static Window window = new Window();
+    public static Properties json = new Properties();
+    public static void handleKeyDown(int key)
+    {
+        if (Keyboard.isKeyDown(key)) {
+            json.setProperty(Keyboard.getKeyName(key).toUpperCase() + ":activated", String.valueOf(true));
+        } else {
+            json.setProperty(Keyboard.getKeyName(key).toUpperCase() + ":activated", String.valueOf(false));
+        }
+    }
+
+    public static ConstantColorConstraint getColor(String str) {
+        if (json.getProperty(str + ":activated") == "true") {
+            return new ConstantColorConstraint(Color.YELLOW);
+        } else {
+            return new ConstantColorConstraint(Color.WHITE);
+        }
+    }
+
+    public static void update()
+    {
+        UIText key_W_text = new UIText("W");
+        key_W_text.setTextScale(new PixelConstraint(2.0F));
+        key_W_text.setX(new PixelConstraint(18.0F));
+        key_W_text.setY(new PixelConstraint(3.0F));
+        key_W_text.setColor(getColor("W"));
+        window.addChild(key_W_text);
+
+        UIText key_S_text = new UIText("S");
+        key_S_text.setTextScale(new PixelConstraint(2.0F));
+        key_S_text.setX(new PixelConstraint(18.0F));
+        key_S_text.setY(new PixelConstraint(23.0F));
+        key_S_text.setColor(getColor("S"));
+        window.addChild(key_S_text);
+
+        UIText key_A_text = new UIText("A");
+        key_A_text.setTextScale(new PixelConstraint(2.0F));
+        key_A_text.setX(new PixelConstraint(3.0F));
+        key_A_text.setY(new PixelConstraint(23.0F));
+        key_A_text.setColor(getColor("A"));
+        window.addChild(key_A_text);
+
+        UIText key_D_text = new UIText("D");
+        key_D_text.setTextScale(new PixelConstraint(2.0F));
+        key_D_text.setX(new PixelConstraint(33.0F));
+        key_D_text.setY(new PixelConstraint(23.0F));
+        key_D_text.setColor(getColor("D"));
+        window.addChild(key_D_text);
+
+        window.draw();
+    }
+}
